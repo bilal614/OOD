4025cc48be39703b01c4d810d43b616a241c7a41
thanhhnk <thanhhnk@gmail.com>
2016-03-29 11:26:30 +0200

    Working on Animal class and add the owner class

--- AnimalShelter/Assignment16_AnimalShelter/Assignment16_AnimalShelter/Classes/Animal.cs
+++ AnimalShelter/Assignment16_AnimalShelter/Assignment16_AnimalShelter/Classes/Animal.cs
@@ -8,5 +8,13 @@ namespace Assignment16_AnimalShelter.Classes
 {
     class Animal
     {
+        private string chipNumber;
+        private double price;
+        private DateTime entryDate;
+        private int days;
+        private string pedigree;
+        private string foundLocation;
+        private bool reverse;
+        private 
     }
 }
