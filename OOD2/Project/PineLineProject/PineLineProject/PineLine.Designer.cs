7b64dc8c46aaa0180a2c221be1d21572e66cf3f2
thanhhnk <thanhhnk@gmail.com>
2016-04-28 13:04:14 +0200

    URS & UID design

--- OOD2/Project/PineLineProject/PineLineProject/PineLine.Designer.cs
+++ OOD2/Project/PineLineProject/PineLineProject/PineLine.Designer.cs
@@ -32,8 +32,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PineLine));
             this.panelLine = new System.Windows.Forms.Panel();
             this.panelLeft = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnSink = new System.Windows.Forms.Button();
+            this.btnAdjustSpliter = new System.Windows.Forms.Button();
             this.btnSpliter = new System.Windows.Forms.Button();
             this.btnMerger = new System.Windows.Forms.Button();
             this.btnPump = new System.Windows.Forms.Button();
@@ -56,119 +57,138 @@
             this.panelLine.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panelLine.Location = new System.Drawing.Point(0, 80);
             this.panelLine.Name = "panelLine";
-            this.panelLine.Size = new System.Drawing.Size(526, 3);
+            this.panelLine.Size = new System.Drawing.Size(583, 3);
             this.panelLine.TabIndex = 1;
             // 
             // panelLeft
             // 
             this.panelLeft.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.panelLeft.Controls.Add(this.button4);
-            this.panelLeft.Controls.Add(this.button3);
+            this.panelLeft.Controls.Add(this.btnLine);
+            this.panelLeft.Controls.Add(this.btnSink);
+            this.panelLeft.Controls.Add(this.btnAdjustSpliter);
             this.panelLeft.Controls.Add(this.btnSpliter);
             this.panelLeft.Controls.Add(this.btnMerger);
             this.panelLeft.Controls.Add(this.btnPump);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelLeft.Location = new System.Drawing.Point(0, 0);
             this.panelLeft.Name = "panelLeft";
-            this.panelLeft.Size = new System.Drawing.Size(107, 389);
+            this.panelLeft.Size = new System.Drawing.Size(107, 518);
             this.panelLeft.TabIndex = 2;
             // 
-            // button4
+            // btnLine
             // 
-            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(12, 314);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(81, 69);
-            this.button4.TabIndex = 18;
-            this.button4.Text = "New";
-            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.btnNew_Click);
+            this.btnLine.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnLine.FlatAppearance.BorderSize = 0;
+            this.btnLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLine.Image = ((System.Drawing.Image)(resources.GetObject("btnLine.Image")));
+            this.btnLine.Location = new System.Drawing.Point(21, 355);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(69, 64);
+            this.btnLine.TabIndex = 18;
+            this.btnLine.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolTip.SetToolTip(this.btnLine, "Pineline");
+            this.btnLine.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnSink
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(12, 239);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 69);
-            this.button3.TabIndex = 18;
-            this.button3.Text = "New";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.btnNew_Click);
+            this.btnSink.AllowDrop = true;
+            this.btnSink.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnSink.FlatAppearance.BorderSize = 0;
+            this.btnSink.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSink.Image = ((System.Drawing.Image)(resources.GetObject("btnSink.Image")));
+            this.btnSink.Location = new System.Drawing.Point(21, 285);
+            this.btnSink.Name = "btnSink";
+            this.btnSink.Size = new System.Drawing.Size(69, 64);
+            this.btnSink.TabIndex = 18;
+            this.btnSink.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolTip.SetToolTip(this.btnSink, "Sink");
+            this.btnSink.UseVisualStyleBackColor = true;
+            this.btnSink.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnSink_MouseDown);
+            // 
+            // btnAdjustSpliter
+            // 
+            this.btnAdjustSpliter.AllowDrop = true;
+            this.btnAdjustSpliter.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnAdjustSpliter.FlatAppearance.BorderSize = 0;
+            this.btnAdjustSpliter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdjustSpliter.Image = ((System.Drawing.Image)(resources.GetObject("btnAdjustSpliter.Image")));
+            this.btnAdjustSpliter.Location = new System.Drawing.Point(21, 215);
+            this.btnAdjustSpliter.Name = "btnAdjustSpliter";
+            this.btnAdjustSpliter.Size = new System.Drawing.Size(69, 64);
+            this.btnAdjustSpliter.TabIndex = 18;
+            this.btnAdjustSpliter.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolTip.SetToolTip(this.btnAdjustSpliter, "Adjustable spliter");
+            this.btnAdjustSpliter.UseVisualStyleBackColor = true;
+            this.btnAdjustSpliter.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnAdjustSpliter_MouseDown);
             // 
             // btnSpliter
             // 
-            this.btnSpliter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSpliter.AllowDrop = true;
             this.btnSpliter.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnSpliter.FlatAppearance.BorderSize = 0;
             this.btnSpliter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSpliter.Image = ((System.Drawing.Image)(resources.GetObject("btnSpliter.Image")));
-            this.btnSpliter.Location = new System.Drawing.Point(12, 164);
+            this.btnSpliter.Location = new System.Drawing.Point(21, 145);
             this.btnSpliter.Name = "btnSpliter";
-            this.btnSpliter.Size = new System.Drawing.Size(81, 69);
+            this.btnSpliter.Size = new System.Drawing.Size(69, 64);
             this.btnSpliter.TabIndex = 18;
             this.btnSpliter.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.toolTip.SetToolTip(this.btnSpliter, "Spliter");
             this.btnSpliter.UseVisualStyleBackColor = true;
-            this.btnSpliter.Click += new System.EventHandler(this.btnNew_Click);
+            this.btnSpliter.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnSpliter_MouseDown);
             // 
             // btnMerger
             // 
-            this.btnMerger.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMerger.AllowDrop = true;
             this.btnMerger.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnMerger.FlatAppearance.BorderSize = 0;
             this.btnMerger.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMerger.Image = ((System.Drawing.Image)(resources.GetObject("btnMerger.Image")));
-            this.btnMerger.Location = new System.Drawing.Point(12, 89);
+            this.btnMerger.Location = new System.Drawing.Point(21, 78);
             this.btnMerger.Name = "btnMerger";
-            this.btnMerger.Size = new System.Drawing.Size(81, 69);
+            this.btnMerger.Size = new System.Drawing.Size(69, 61);
             this.btnMerger.TabIndex = 18;
             this.btnMerger.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.toolTip.SetToolTip(this.btnMerger, "Merger");
             this.btnMerger.UseVisualStyleBackColor = true;
-            this.btnMerger.Click += new System.EventHandler(this.btnNew_Click);
+            this.btnMerger.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnMerger_MouseDown);
             // 
             // btnPump
             // 
-            this.btnPump.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnPump.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnPump.FlatAppearance.BorderSize = 0;
             this.btnPump.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPump.Image = ((System.Drawing.Image)(resources.GetObject("btnPump.Image")));
-            this.btnPump.Location = new System.Drawing.Point(12, 14);
+            this.btnPump.Location = new System.Drawing.Point(21, 12);
             this.btnPump.Name = "btnPump";
-            this.btnPump.Size = new System.Drawing.Size(81, 69);
+            this.btnPump.Size = new System.Drawing.Size(69, 60);
             this.btnPump.TabIndex = 18;
             this.btnPump.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.toolTip.SetToolTip(this.btnPump, "Pump");
             this.btnPump.UseVisualStyleBackColor = true;
-            this.btnPump.Click += new System.EventHandler(this.btnNew_Click);
+            this.btnPump.Click += new System.EventHandler(this.btnPump_Click);
+            this.btnPump.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnPump_MouseDown);
             // 
             // panelLineLeft
             // 
             this.panelLineLeft.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panelLineLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLineLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLineLeft.Location = new System.Drawing.Point(107, 0);
             this.panelLineLeft.Name = "panelLineLeft";
-            this.panelLineLeft.Size = new System.Drawing.Size(3, 389);
+            this.panelLineLeft.Size = new System.Drawing.Size(3, 518);
             this.panelLineLeft.TabIndex = 3;
             // 
             // panelDrawing
             // 
+            this.panelDrawing.AllowDrop = true;
+            this.panelDrawing.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panelDrawing.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelDrawing.Location = new System.Drawing.Point(0, 0);
             this.panelDrawing.Name = "panelDrawing";
-            this.panelDrawing.Size = new System.Drawing.Size(529, 389);
+            this.panelDrawing.Size = new System.Drawing.Size(583, 518);
             this.panelDrawing.TabIndex = 4;
+            this.panelDrawing.DragDrop += new System.Windows.Forms.DragEventHandler(this.panelDrawing_DragDrop);
+            this.panelDrawing.DragEnter += new System.Windows.Forms.DragEventHandler(this.panelDrawing_DragEnter);
             // 
             // panelTop
             // 
@@ -178,9 +198,9 @@
             this.panelTop.Controls.Add(this.btnNew);
             this.panelTop.Controls.Add(this.btnOpen);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(3, 0);
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(526, 83);
+            this.panelTop.Size = new System.Drawing.Size(583, 83);
             this.panelTop.TabIndex = 0;
             // 
             // btnSave
@@ -190,7 +210,7 @@
             this.btnSave.FlatAppearance.BorderSize = 0;
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
-            this.btnSave.Location = new System.Drawing.Point(464, 5);
+            this.btnSave.Location = new System.Drawing.Point(521, 5);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(50, 69);
             this.btnSave.TabIndex = 18;
@@ -205,7 +225,7 @@
             this.btnNew.FlatAppearance.BorderSize = 0;
             this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNew.Image = ((System.Drawing.Image)(resources.GetObject("btnNew.Image")));
-            this.btnNew.Location = new System.Drawing.Point(354, 5);
+            this.btnNew.Location = new System.Drawing.Point(411, 5);
             this.btnNew.Name = "btnNew";
             this.btnNew.Size = new System.Drawing.Size(49, 69);
             this.btnNew.TabIndex = 18;
@@ -220,7 +240,7 @@
             this.btnOpen.FlatAppearance.BorderSize = 0;
             this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
-            this.btnOpen.Location = new System.Drawing.Point(409, 5);
+            this.btnOpen.Location = new System.Drawing.Point(466, 5);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(49, 69);
             this.btnOpen.TabIndex = 18;
@@ -231,12 +251,11 @@
             // panelRightFill
             // 
             this.panelRightFill.Controls.Add(this.panelTop);
-            this.panelRightFill.Controls.Add(this.panelLineLeft);
             this.panelRightFill.Controls.Add(this.panelDrawing);
             this.panelRightFill.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelRightFill.Location = new System.Drawing.Point(107, 0);
             this.panelRightFill.Name = "panelRightFill";
-            this.panelRightFill.Size = new System.Drawing.Size(529, 389);
+            this.panelRightFill.Size = new System.Drawing.Size(583, 518);
             this.panelRightFill.TabIndex = 5;
             // 
             // PineLine
@@ -244,7 +263,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(636, 389);
+            this.ClientSize = new System.Drawing.Size(690, 518);
+            this.Controls.Add(this.panelLineLeft);
             this.Controls.Add(this.panelRightFill);
             this.Controls.Add(this.panelLeft);
             this.Name = "PineLine";
@@ -268,12 +288,13 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.Panel panelRightFill;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnSink;
+        private System.Windows.Forms.Button btnAdjustSpliter;
         private System.Windows.Forms.Button btnSpliter;
         private System.Windows.Forms.Button btnMerger;
         private System.Windows.Forms.ToolTip toolTip;
         private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnLine;
 
     }
 }
--- OOD2/Project/PineLineProject/PineLineProject/PineLine.cs
+++ OOD2/Project/PineLineProject/PineLineProject/PineLine.cs
@@ -19,12 +19,62 @@ namespace PineLineProject
 
         private void PineLine_Load(object sender, EventArgs e)
         {
+
         }
 
-        private void btnNew_Click(object sender, EventArgs e)
+        private void btnPump_Click(object sender, EventArgs e)
         {
 
         }
-       
+        #region Drag and drop components implementation
+
+        private void btnPump_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Drag the image of the button
+            btnPump.DoDragDrop(btnPump.Image, DragDropEffects.Move);
+        }
+
+        private void panelDrawing_DragEnter(object sender, DragEventArgs e)
+        {
+            //Allow every types of objects. Need to be improved. Only allow Bitmap object
+            e.Effect = e.AllowedEffect;
+        }
+
+        private void panelDrawing_DragDrop(object sender, DragEventArgs e)
+        {
+            //Get the image from data and set it to picture box
+            PictureBox pic = new PictureBox();
+            pic.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            //Draw the image in the position of the cursor
+            int xPos = e.X;
+            int yPos = e.Y;
+
+            Point clientPoint = panelDrawing.PointToClient(new Point(e.X, e.Y));
+
+            Graphics gr = panelDrawing.CreateGraphics();
+            gr.DrawImage(pic.Image, clientPoint.X, clientPoint.Y);
+        }
+
+        private void btnMerger_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnMerger.DoDragDrop(btnMerger.Image, DragDropEffects.Move);
+        }
+        #endregion
+
+        private void btnSpliter_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnSpliter.DoDragDrop(btnSpliter.Image, DragDropEffects.Move);
+        }
+
+        private void btnAdjustSpliter_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnAdjustSpliter.DoDragDrop(btnAdjustSpliter.Image, DragDropEffects.Move);
+        }
+
+        private void btnSink_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnSink.DoDragDrop(btnSink.Image, DragDropEffects.Move);
+        }
+        
     }
 }
