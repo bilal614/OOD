9c6f5af28139ffd446a2163fcfd31cc643fdb1d8
thanhhnk <thanhhnk@gmail.com>
2016-06-15 14:17:17 +0200

    Working on unit test

--- OOD2/Project/PipeLine_System/UnitTestProject_Pipeline/FileHandlerTest.cs
+++ OOD2/Project/PipeLine_System/UnitTestProject_Pipeline/FileHandlerTest.cs
@@ -13,23 +13,21 @@ namespace UnitTestProject_Pipeline
         public void TestWriteToFile()
         { 
             FileHandler f = new FileHandler("../../PipeLine_System/NetworkFiles/Network_02.txt");
-            List<Component> components = new List<Component>();
+            Network nw = new Network();
             Component p = new Pump(1, new Point(100, 200), 150.5);
-            components.Add(p);
+            nw.Addcomponent(p);
             p = new Sink(2, new Point(253, 500), 350);
-            components.Add(p);
+            nw.Addcomponent(p);
             p = new Merger(3, new Point(200, 300), 150);
-            components.Add(p);
+            nw.Addcomponent(p);
             p = new Spliter(4, new Point(300, 205), 500);
-            components.Add(p);
+            nw.Addcomponent(p);
             p = new AdjustableSpliter(5, new Point(300, 500), 30, 56);
             List<PipeLine> pipelines = new List<PipeLine>();
             PipeLine pi = new PipeLine(6,100);
-            pipelines.Add(pi);
+            //pipelines.Add(pi);
             pi = new PipeLine(100, 200);
-            pipelines.Add(pi);
-
-            Network nw = new Network();
+            //pipleines.Add(pi);
             f.WriteToFile(nw);
         }
             
